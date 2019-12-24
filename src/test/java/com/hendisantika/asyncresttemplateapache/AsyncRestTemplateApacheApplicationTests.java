ca1dbb9654f76933fba6f84f616dc657883b6f00
Hendi Santika <hendisantika@gmail.com>
2019-12-24 19:07:32 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/asyncresttemplateapache/AsyncRestTemplateApacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.asyncresttemplateapache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncRestTemplateApacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
